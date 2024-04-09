f54213388ab217d6d48d55102c58f2f3fe1e8343
abhay-raj19 <rajpootabhay423@gmail.com>
2024-04-09 06:34:39 +0530

    feat:Socket_connection
    Signed-off-by: abhay-raj19 <rajpootabhay423@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,48 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import "./App.css";
 
- export default function App() {
-  const [socket, setSocket] = useState<null | WebSocket>(null)
+export default function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null);
   const [message, setMessage] = useState(" ");
-  const [clientmessage,setClientmessage] = useState(" ");
-
+  const [clientmessage, setClientmessage] = useState(" ");
 
   useEffect(() => {
-  const socket = new WebSocket("ws://localhost:8080")
-  socket.onopen = () =>{
-    console.log("CONNECTED");
-    setSocket(socket)
-    }
-  socket.onmessage = (message) =>{
-    console.log("Recieved message",message.data)
-    setMessage(message.data)
-  }
-  }, [])
-  
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log("CONNECTED");
+      setSocket(socket);
+    };
+    socket.onmessage = (message) => {
+      console.log("Recieved message", message.data);
+      setMessage(message.data);
+    };
+  }, []);
 
-  if(!socket){
-    return (
-      <div>
-        Connecting to Socket server... 
-      </div>
-    )
+  if (!socket) {
+    return <div>Connecting to Socket server...</div>;
   }
   return (
     <>
-    <input type="text" onChange={(e) => {
-      setClientmessage(e.target.value)
-    }}/>
-    <button type="button" onClick={() =>{
-      socket.send(clientmessage)
-    }}>SEND</button>
-    {message}       
+      <div className="">
+        <span>Message to be sent:</span>
+
+        <input
+          type="text"
+          onChange={(e) => {
+            setClientmessage(e.target.value);
+          }}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            socket.send(clientmessage);
+          }}
+        >
+          SEND
+        </button>
+        {message}
+      </div>
     </>
-  )
+  );
 }
-
-
-
